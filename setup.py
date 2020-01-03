10d9143b74936ade5ff7bc7318afcdc434edda80
Manuel Pepe <manuel.pepe@avature.net>
2020-01-03 15:30:53 -0300

    RTD docs

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Turnable',
+      version='0.1',
+      description='Turn-based game micro-framework',
+      author='Manuel PEpe',
+      author_email='manuelpepe-dev@outlook.com.ar',
+      packages=['turnable'])
